c5866508f81c465572a2bd64d9e480e31372410e
lmpham1 <est2000vn@gmail.com>
2023-11-16 04:57:30 +0000

    update token counting logic

--- src/functions/generateAnswer.js
+++ src/functions/generateAnswer.js
@@ -1,11 +1,12 @@
 const { makeAPICall } = require('../api');
-const { get_encoding } = require('tiktoken');
+const { encoding_for_model } = require('tiktoken');
 const logger = require('../logger');
 
 async function generateAnswer(prompt, username = '') {
   try {
     const model = process.env.OPENAI_MODEL;
-    const enc = get_encoding(model);
+    logger.debug(model);
+    const enc = encoding_for_model(model);
     const token = enc.encode(prompt);
     logger.info(`Request created with ${token.length} tokens.`);
     const response = await makeAPICall(prompt);
@@ -13,6 +14,7 @@ async function generateAnswer(prompt, username = '') {
       username ? `@${username} said: ${prompt}\n\n` : ''
     }${response}`;
     logger.info('Request completed.');
+    enc.free();
     return responseWithUser;
   } catch (err) {
     logger.error('Error generating answer');
