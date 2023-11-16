09b440926df572f06f4e9ddc55db4635738b6c24
Minh Pham <est2000vn@gmail.com>
2023-11-15 23:35:09 -0500

    add logs for number of tokens requested

--- src/functions/generateAnswer.js
+++ src/functions/generateAnswer.js
@@ -1,11 +1,24 @@
 const { makeAPICall } = require('../api');
+const { get_encoding } = require('tiktoken');
+const logger = require('../logger');
 
 async function generateAnswer(prompt, username = '') {
-  const response = await makeAPICall(prompt);
-  const responseWithUser = `${
-    username ? `@${username} said: ${prompt}\n\n` : ''
-  }${response}`;
-  return responseWithUser;
+  try {
+    const model = process.env.OPENAI_MODEL;
+    const enc = get_encoding(model);
+    const token = enc.encode(prompt);
+    logger.info(`Request created with ${token.length} tokens.`);
+    const response = await makeAPICall(prompt);
+    const responseWithUser = `${
+      username ? `@${username} said: ${prompt}\n\n` : ''
+    }${response}`;
+    logger.info('Request completed.');
+    return responseWithUser;
+  } catch (err) {
+    logger.error('Error generating answer');
+    logger.error(err);
+    throw err;
+  }
 }
 
 module.exports = generateAnswer;
